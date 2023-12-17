f0973872b60f9ad7faa939a7c23ce1c16b90b63f
gw <932150283@qq.com>
2023-12-17 09:33:32 +0800

    cpp_string_class_1217

---
+++ 12code/1202/swap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(/* 补全参数 */);
+
+int main ()
+{
+    int a = 1;
+    int b = 2;
+    swap(/* 补全参数 */);
+    printf("a： %d\n", a);
+    printf("a： %d\n", b);
+    
+    return 0;
+}
+
+void swap(/* 补全参数 */){
+    /* 补全函数体，实现交换两个int的值 */
+}
+void swap1(int *a, int *b);
+void swap1(int *a, int *b){
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
